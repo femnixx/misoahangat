b00749db11dad0d1ccaf3a453dff5a97d111815f
femnixx <femnitality@gmail.com>
2026-05-22 15:19:02 +0700

    feat: add broken navbar component

--- lib/components/Navbar.dart
+++ lib/components/Navbar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:misoahangat/pages/Beranda.dart';
+import 'package:misoahangat/pages/Pesanan.dart';
+import 'package:misoahangat/pages/Profil.dart';
+import 'package:misoahangat/pages/Voucher.dart';
 
 class Navbar extends StatefulWidget {
   const Navbar({super.key});
@@ -10,12 +14,80 @@ class Navbar extends StatefulWidget {
 class _NavbarState extends State<Navbar> {
 
   int _currentIndex = 0;
+  String title = "Homepage";
 
-  List<Widget> Pages = [
-
+  final List<Widget> _pages = [
+    Beranda(),
+    Pesanan(),
+    Voucher(),
+    Profil()
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+          color: Colors.white,
+        ),
+        child: SafeArea(
+          child: SizedBox(
+            height: 65,
+            child: Row(
+              children: [
+                ...navComponents.map(
+                  (items) => Expanded(
+                    child: Material(
+                      color: _currentIndex == items.currentIndex ? Colors.red : Colors.transparent,
+                      child: InkWell(
+                        onTap: () => {
+                          setState(() {
+                              _currentIndex = items.currentIndex;
+                          }),
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(items.icon, color: Colors.redAccent),
+                            Text(
+                              items.name,
+                              style: TextStyle(
+                                color: Colors.redAccent
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ) 
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class NavButtons { 
+  String name;
+  IconData icon;
+  int currentIndex;
+
+  NavButtons({required this.name, required this.icon, required this.currentIndex}); 
+}
+
+List<NavButtons> navComponents = [
+  NavButtons(name: 'Beranda', icon: Icons.home, currentIndex: 0),
+  NavButtons(name: 'Pesanan', icon: Icons.receipt, currentIndex: 1),
+  NavButtons(name: 'Voucher', icon: Icons.discount, currentIndex: 2),
+  NavButtons(name: 'Profil', icon: Icons.person, currentIndex: 3)
+];
+
---
+++ lib/pages/Voucher.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Voucher extends StatefulWidget {
+  const Voucher({super.key});
+
+  @override
+  State<Voucher> createState() => _VoucherState();
+}
+
+class _VoucherState extends State<Voucher> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
