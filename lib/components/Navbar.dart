15b658aa28a9f80f30ba4fcd42ec76ac2fbefd94
femnixx <femnitality@gmail.com>
2026-05-22 14:26:30 +0700

    feat: add Navbar, Beranda, Pesanan, Profil & Voucher page

---
+++ lib/components/Navbar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+
+  int _currentIndex = 0;
+
+  List<Widget> Pages = [
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
